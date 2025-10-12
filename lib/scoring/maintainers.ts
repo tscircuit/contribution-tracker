fce1e539cd244dbdee6ff19702ab7edb05c0e409
Severin Ibarluzea <seveibar@gmail.com>
2025-10-12 10:49:23 -0700

    Upgrade Abse from maintainer1 to maintainer2

--- lib/scoring/maintainers.ts
+++ lib/scoring/maintainers.ts
@@ -1,6 +1,6 @@
 export const MAINTAINERS = {
   ShiboSoftwareDev: "maintainer3",
   "ArnavK-09": "maintainer3",
-  Abse2001: "maintainer1",
+  Abse2001: "maintainer2",
   MustafaMulla29: "maintainer1",
 }
