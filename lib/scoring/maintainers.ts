c66e4abf8e8ba93acae27f9f9cdcb42102567183
Arnav K <arnavkaushal09@gmail.com>
2025-10-27 19:22:17 +0530

    Update maintainer roles and icons in MaintainersList component

--- lib/scoring/maintainers.ts
+++ lib/scoring/maintainers.ts
@@ -1,6 +1,6 @@
 export const MAINTAINERS = {
   ShiboSoftwareDev: "maintainer3",
-  "ArnavK-09": "maintainer3",
+  "ArnavK-09": "maintainer4",
   Abse2001: "maintainer2",
   MustafaMulla29: "maintainer1",
   techmannih: "maintainer1",
