7908bf0cb77fc74d402a542492333ddaf3c04312
Shibo <shibosoftwaredev@gmail.com>
2025-11-25 19:19:39 +0200

    shibo to maintainer4 (#266)

--- lib/scoring/maintainers.ts
+++ lib/scoring/maintainers.ts
@@ -1,5 +1,5 @@
 export const MAINTAINERS = {
-  ShiboSoftwareDev: "maintainer3",
+  ShiboSoftwareDev: "maintainer4",
   "ArnavK-09": "maintainer3",
   Abse2001: "maintainer2",
   MustafaMulla29: "maintainer1",
