976ea915f4d3c76cf28fc3eea76b64ab09900a61
Severin Ibarluzea <seveibar@gmail.com>
2025-12-10 19:15:41 -0800

    Downgrade maintainers w/o anchor/offset display contribution

--- lib/scoring/maintainers.ts
+++ lib/scoring/maintainers.ts
@@ -1,8 +1,5 @@
 export const MAINTAINERS = {
-  ShiboSoftwareDev: "maintainer4",
-  "ArnavK-09": "maintainer3",
-  Abse2001: "maintainer2",
-  MustafaMulla29: "maintainer1",
-  techmannih: "maintainer1",
-  rushabhcodes: "maintainer1",
+  ShiboSoftwareDev: "maintainer3",
+  "ArnavK-09": "maintainer2",
+  Abse2001: "maintainer1",
 }
