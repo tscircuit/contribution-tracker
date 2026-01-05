9ee3d8a36bc0043549b520f43b6658489812afee
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2026-01-05 11:59:32 +0530

    Remove MustafaMulla29 from maintainers list (#291)

--- lib/scoring/maintainers.ts
+++ lib/scoring/maintainers.ts
@@ -2,7 +2,6 @@ export const MAINTAINERS = {
   ShiboSoftwareDev: "maintainer4",
   "ArnavK-09": "maintainer3",
   Abse2001: "maintainer2",
-  MustafaMulla29: "maintainer1",
   techmannih: "maintainer1",
   rushabhcodes: "maintainer1",
 }
